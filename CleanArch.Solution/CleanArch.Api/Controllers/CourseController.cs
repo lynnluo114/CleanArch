a423f656efd960553bec9546f0aabedb8d9b61dd
lynnluo114 <lynn.luo1104@gmail.com>
2022-03-07 12:40:05 -0500

    Implementation of Api

---
+++ CleanArch.Solution/CleanArch.Api/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArch.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCourse([FromBody] CourseViewModel courseViewModel)
+        {
+            _courseService.Create(courseViewModel);
+            return Ok(courseViewModel);
+        }
+    }
+}
